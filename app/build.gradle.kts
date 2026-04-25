cfa443dde301793ba36e7a4c4f141af131b0aa95
Ryuuko <1712538688@qq.com>
2026-04-25 23:35:29 +0800

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.durian.game"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.durian.game"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation("org.java-websocket:Java-WebSocket:1.5.3")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+}
